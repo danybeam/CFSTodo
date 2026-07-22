781dff412f309023c48a1b7752b330e6df30a483
Daniel Orozco <danybeam@gmail.com>
2026-07-22 16:58:53 -0400

    Update ES target

--- src/components/ActiveTask.tsx
+++ src/components/ActiveTask.tsx
@@ -6,12 +6,12 @@ import { Task } from "../models/Task";
 export function ActiveTask() {
     const [firstTask, setFirstTask] = createSignal<Task>(TaskContext.tasks[0]);
     createEffect(() => {
-        setFirstTask(TaskContext.tasks.at(0));
+        setFirstTask(TaskContext.tasks.at(0) ?? { id: -1, text: "error", completed: false });
     })
 
     return (
         <Show
-            when={firstTask()}
+            when={firstTask()?.id >= 0}
             fallback={<span>Add a task to get started</span>}
         >
             <TaskListItem task={firstTask()} />
