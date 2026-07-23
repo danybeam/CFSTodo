9178ed112d82eed22e4d46f0500bd240e60c6904
Daniel Orozco <danybeam@gmail.com>
2026-07-23 14:12:50 -0400

    Stop showing active task in header if none are active

--- src/components/CurrentTask.tsx
+++ src/components/CurrentTask.tsx
@@ -6,13 +6,13 @@ import { Task } from "../models/Task";
 export default function CurrentTask() {
     const [firstTask, setFirstTask] = createSignal<Task>(TaskContext.tasks[0]);
     createEffect(() => {
-        setFirstTask(TaskContext.tasks.at(0) ?? { id: -1, text: "error", completed: false, isSuspended: false });
+        setFirstTask(TaskContext.tasks.at(0) ?? { id: -1, text: "error", completed: false, isSuspended: false, vruntime: 0 });
     })
 
     return (
         <Show
-            when={firstTask()?.id >= 0}
-            fallback={<span>Add a task to get started</span>}
+            when={firstTask()?.id >= 0 && !firstTask().completed}
+            fallback={<span>{firstTask().completed ? "All task are completed. Add another task to get started." : "Add a task to get started"}</span>}
         >
             <TaskListItem task={firstTask()} isCurrentTask={true} />
         </Show>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,17 @@
-import { For } from "solid-js";
+import { createEffect, createSignal, For } from "solid-js";
 import TaskListItem from "./TaskListItem";
 import TaskContext from "./context/GlobalTaskList"
 
 export default function TaskList() {
     const { tasks } = TaskContext;
 
+    const [allComplete, setAllComplete] = createSignal(false);
+    createEffect(() => {
+        setAllComplete(tasks.at(0)?.completed ?? false);
+    })
+
     return (
-        <For each={tasks.slice(1)}>{
+        <For each={tasks.slice(allComplete() ? 0 : 1)}>{
             (task) => <TaskListItem task={task} isCurrentTask={false} />
         }
         </For>
