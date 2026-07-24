aa6e3fb3591faf538bdfc1e9879a7078b5e192f5
Daniel Orozco <danybeam@gmail.com>
2026-07-24 11:22:26 -0400

    Change default value of time entry to time slice

--- src/components/TimeEntry.tsx
+++ src/components/TimeEntry.tsx
@@ -1,5 +1,7 @@
 import { createSignal } from "solid-js";
 
+import AppContext from "./context/AppSettings"
+
 type TimeEntryProps = {
     timeEntryCallback: (hrs: number) => void
     requestSuspendCallback: (suspend: boolean) => void
@@ -18,14 +20,14 @@ export default function TimeEntry(props: TimeEntryProps) {
                 setTimeout(() => {
                     props.timeEntryCallback(hours);
                     props.requestSuspendCallback(button == "suspend");
-                }, 500);
+                }, 100);
                 setPopup(false);
             }}>
             <input
                 type="number"
                 id="real-hrs"
                 name="hr-input"
-                value="1"
+                value={AppContext.calculatedTimeSlice()}
             />
             <button type="submit" name="action" style="width: fit-content" value="rotate">Rotate Task</button>
             <button type="submit" name="action" style="width: fit-content" value="suspend">Rotate Task</button>
